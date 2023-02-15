be6b56bcdbb4d98a0a0902d78c5d562a7159a460
arpit-batra <arpitbatra98@gmail.com>
2023-02-15 18:08:59 +0530

    Added Animation for intro page

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -5,6 +5,7 @@ import 'package:my_portfolio/sections/about_me_section.dart';
 import 'package:my_portfolio/sections/projects_section.dart';
 import 'package:my_portfolio/sections/skills_section.dart';
 import 'package:my_portfolio/sections/slant_box.dart';
+import 'package:my_portfolio/widgets/navbar/animated_navbar.dart';
 import 'package:my_portfolio/widgets/navbar/navbar.dart';
 
 class MainPage extends StatelessWidget {
@@ -46,7 +47,7 @@ class MainPage extends StatelessWidget {
                   ],
                 ),
               ),
-              NavBar(
+              AnimatedNavBar(
                 controller,
                 aboutSectionKey: aboutSectionKey,
                 skillsSectionKey: skillsSectionKey,
--- lib/sections/intro.dart
+++ lib/sections/intro.dart
@@ -1,7 +1,37 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
-class Intro extends StatelessWidget {
+class Intro extends StatefulWidget {
   const Intro({Key? key}) : super(key: key);
+
+  @override
+  State<Intro> createState() => _IntroState();
+}
+
+class _IntroState extends State<Intro> with TickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation _translationAnimation;
+  late Animation<double> _fadeAnimation;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 1500));
+
+    _translationAnimation =
+        Tween<Offset>(begin: const Offset(-100, 0), end: const Offset(0, 0))
+            .animate(CurvedAnimation(
+                parent: _animationController, curve: Curves.fastOutSlowIn));
+
+    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+        CurvedAnimation(
+            parent: _animationController, curve: Curves.fastOutSlowIn));
+
+    _animationController.forward();
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,32 +49,43 @@ class Intro extends StatelessWidget {
         height: deviceHeight,
         padding: const EdgeInsets.all(32),
         width: deviceWidth > prefferedWidth ? prefferedWidth : deviceWidth,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Flex(
-              direction: deviceWidth > prefferedWidth
-                  ? Axis.horizontal
-                  : Axis.vertical,
-              // mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  "Hi, I'm ",
-                  style: introTextStyle,
+        child: AnimatedBuilder(
+          animation: _translationAnimation,
+          builder: ((context, child) {
+            return Transform.translate(
+              offset: _translationAnimation.value,
+              child: FadeTransition(
+                opacity: _fadeAnimation,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Flex(
+                      direction: deviceWidth > prefferedWidth
+                          ? Axis.horizontal
+                          : Axis.vertical,
+                      // mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "Hi, I'm ",
+                          style: introTextStyle,
+                        ),
+                        Text(
+                          "Arpit.",
+                          style: nameTextStyle,
+                        )
+                      ],
+                    ),
+                    const Text(
+                      "I'm a Flutter Developer.",
+                      style: introTextStyle,
+                    )
+                  ],
                 ),
-                Text(
-                  "Arpit.",
-                  style: nameTextStyle,
-                )
-              ],
-            ),
-            const Text(
-              "I'm a Flutter Developer.",
-              style: introTextStyle,
-            )
-          ],
+              ),
+            );
+          }),
         ),
       ),
     );
